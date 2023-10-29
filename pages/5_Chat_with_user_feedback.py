514351eb6290ff68741be5716c07a172aa3bb3cd
tapiwa chamboko <80702706+tapiwachamb@users.noreply.github.com>
2023-10-29 13:07:49 +0200

    Update 5_Chat_with_user_feedback.py

--- pages/5_Chat_with_user_feedback.py
+++ pages/5_Chat_with_user_feedback.py
@@ -7,7 +7,7 @@ with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="feedback_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
     "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/5_Chat_with_user_feedback.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+    
 
 st.title("📝 Chat with feedback (Trubrics)")
 
