8e24e5960fea6ad062de325c682321755180f6a3
tapiwa chamboko <80702706+tapiwachamb@users.noreply.github.com>
2023-10-29 13:05:04 +0200

    Update Chatbot.py

--- Chatbot.py
+++ Chatbot.py
@@ -4,11 +4,11 @@ import streamlit as st
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
     "[Get an OpenAI API key](https://platform.openai.com/account/api-keys)"
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/Chatbot.py)"
+    "[View the source code](https://github.com/tapiwachamb/LLM-Chatbot/blob/main/Chatbot.py)"
     "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
 
 st.title("💬 Chatbot")
-st.caption("🚀 A streamlit chatbot powered by OpenAI LLM")
+st.caption("🚀 A streamlit chatbot powered by OpenAI LLM made by Tapiwa Chamboko")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
 
